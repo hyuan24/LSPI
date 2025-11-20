ed328867983ac3b2d5e3f80442f7844151c27833
Helen Yuan <165934908+hyuan24@users.noreply.github.com>
2025-11-19 17:02:44 -0800

    updates to action plot

--- run.py
+++ run.py
@@ -99,37 +99,48 @@ def experiment_2(numPol, maxEps, testEps, basisType="radial", reward="sutton_bar
 
     return totalAvg
 
-def plot_actions(numEps, numTicks, basisType="radial", reward="sutton_barto", alpha=1.0):
+def plot_actions(numEps, numTicks, numPol, basisType="radial", reward="sutton_barto", alpha=1.0):
    
     env = ModifiedCartPoleEnv(reward)
     action_dim = 1
     obs_dim = 2
     num_basis = 10 # PER BLOCK
 
-    memory = Memory(numEps * 10, action_dim, obs_dim)  
-    memory, avg_random_steps = collect_data(env, memory, numEps, 1)
-    agent = LSPI(env.action_space.n, num_basis, env, env.observation_space.shape[0], basisType, alpha)
-    sample = memory.select_sample(round(numEps*avg_random_steps))  # [current_state, actions, rewards, next_state, done]
-    _ = agent.train(sample, LSPI_ITERATION)
+    memory = Memory(numEps * 10*numPol, action_dim, obs_dim)  
+    memory, avg_random_steps = collect_data(env, memory, numEps, numPol)
 
-    A = np.zeros([numTicks, numTicks])
     actions = [0,1,2]
-    weights = agent.policy.weights
-
     angles = np.linspace(-np.pi/2, np.pi/2, numTicks)
-    angle_dots = np.linspace(-5, 5, numTicks)
+    angles_dot = np.linspace(-5, 5, numTicks)
 
-    for i, a in enumerate(angles):
-        for j, ad in enumerate(angle_dots):
-            state = [a,ad] 
-            qs = [np.dot(agent.policy.basis_function.basisfunc(state, ac), weights) for ac in actions]
-            A[i,j] = np.argmax(qs)
-        
-    #plt.figure(figsize=(8, 6)) 
-    ax = sns.heatmap(A.T, fmt=".1f", cmap="viridis", linewidths=.5)
-    #ax.set_xticks(angles)
-    #ax.set_yticks(angle_dots)
-    plt.title("learned actions")
+    As = []
+   
+    for _ in range(numPol):
+        agent = LSPI(env.action_space.n, num_basis, env, env.observation_space.shape[0], basisType, alpha)
+        sample = memory.select_sample(round(numEps*avg_random_steps))  # [current_state, actions, rewards, next_state, done]
+        _ = agent.train(sample, LSPI_ITERATION)
+        weights = agent.policy.weights
+
+        A = np.zeros([numTicks,numTicks])
+
+        for i, a in enumerate(angles):
+            for j, ad in enumerate(angles_dot):
+                state = [a,ad] 
+                qs = [np.dot(agent.policy.basis_function.basisfunc(state, ac), weights) for ac in actions]
+                A[i,j] = np.argmax(qs)
+        As.append(A)
+
+    fig, axs = plt.subplots(1,numPol,figsize=(4*numPol,4))
+    
+    for k, (ax,A) in enumerate(zip(axs,As)):
+        im = ax.imshow(A, origin="lower",extent=[angles[0], angles[-1], angles_dot[0], angles_dot[-1]], aspect='auto')
+        if k ==0:
+            ax.set_ylabel("angular velocity (rad/s)")
+        ax.set_xlabel("angle (rad)")
+        ax.set_title(f"policy {k+1}")
+    
+    fig.colorbar(im, ax=axs.ravel().tolist(), label="pi(angle, angular velocity)")
+    #plt.tight_layout()
     plt.show()
         
   
@@ -139,7 +150,7 @@ def plot_actions(numEps, numTicks, basisType="radial", reward="sutton_barto", al
 def main():
 
     #test1 = experiment_2(10, 300, 100, "radial", "sutton_barto", alpha=1.0) 
-    plot_actions(300, 50)
+    plot_actions(500, 100, 4)
     #data = np.column_stack((poly,poly))
     #np.savetxt("results.txt", data, header="poly poly", comments='')
 
